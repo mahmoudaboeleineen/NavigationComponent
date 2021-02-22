e9e89208660910bacedb9c2e12b5f4e2306d3e50
maboe <maboelenen55@gmail.com>
2021-02-22 03:16:43 +0200

    add Deep Links

--- app/src/main/java/com/mahmoudroid/navigationcomponent/LoginFragment.kt
+++ app/src/main/java/com/mahmoudroid/navigationcomponent/LoginFragment.kt
@@ -4,11 +4,18 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
+
+    private val args: LoginFragmentArgs by navArgs()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val usernameDeepLink = args.username
+        editText_userName.setText(usernameDeepLink)
 
         button_confirm.setOnClickListener {
 
